c636d72d6886e5b8bfa34d13b5e1f9ed6b46f33b
RafaelVasquezRV <arcangelprot@gmail.com>
2022-08-29 18:21:53 -0400

    Configuración inicial del proyecto 291821AGO2022

---
+++ setting_djmarket/settings/local.py
@@ -0,0 +1,13 @@
+from  .base import *
+from .db import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = DB_POSTGRESQL
+
